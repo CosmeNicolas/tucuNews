fee311449804b8f9a404f9ac94e9b9c89f794861
CosmeNicolas <nicomas7590@gmail.com>
2024-01-11 22:14:36 -0300

    agregue el componente News.jsx para consumir la API de newsdata.io

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 import { Container, Image } from 'react-bootstrap'
 import Footer from "./components/Footer"
+import News from "./components/News"
 import TucuNews from "./assets/TheTucu-News-sf.png"
+
+
 function App() {
 
   return (
     <>
      <Container  className="main">
       
-      <div className="mt-5" >
+      <div className="mt-5 text-center" >
         <Image src={TucuNews} fluid />
       </div>
-      
+      <News/>
      </Container>
      <Footer/>
     </>
