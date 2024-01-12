e37d7176b48934d69fb4aee9e8d982104312cd34
CosmeNicolas <nicomas7590@gmail.com>
2024-01-12 14:18:50 -0300

    cambie el section por una card de react-bootstrap

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Image } from 'react-bootstrap';
 
 const News = ({noticias}) => {
   return (
     <Container>
       {
         noticias.map(noticia => (
-      <section key={noticias.id}>
-      <h1>{noticia.title}</h1>
-      <br />
-      <p>
-        {noticia.description}
-      </p>
-      </section>
+          <Card key={noticia.article_id} style={{ width: '18rem' }}>
+           <Image src={noticia.image_url} /> 
+            <Card.Body>
+              <Card.Title>{noticia.title}</Card.Title>
+              <Card.Text>
+               {noticia.description}
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
         ))
       }
     </Container>
