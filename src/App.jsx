5fc3a904f4885a9a6e43284e7591b12675cf533e
CosmeNicolas <nicomas7590@gmail.com>
2024-01-12 14:33:26 -0300

    agreggue bloque de try y catch

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,26 @@ function App() {
   
   /* hago la peticion HTTP GET cons Async y Await */
   const consultaApi = async ()=>{
-    const respuesAPI = await fetch('https://newsdata.io/api/1/news?apikey=pub_3632485f1de71f34f4409a4afc369bf6be35d&q=pizza');
-    /* extraigo los datos del body de la peticion  */
-    const datos = await respuesAPI.json()
-    console.log(respuesAPI)
-    console.log(datos)
-    /* me dio de resultado el array q es lo q esos pasos saben devolver , son 10 como especifica en este caso esta API de noticias  */
-    setNoticias(datos.results)
+
+    /* usamosTry/Catch para el manejo de errores  */
+    /* tambien se usa en el backend */
+    try {
+      const respuesAPI = await fetch('https://newsdata.io/api/1/news?apikey=pub_3632485f1de71f34f4409a4afc369bf6be35d&q=pizza');
+      /* extraigo los datos del body de la peticion  */
+      const datos = await respuesAPI.json()
+      console.log(respuesAPI)
+      console.log(datos)
+      /* me dio de resultado el array q es lo q esos pasos saben devolver , son 10 como especifica en este caso esta API de noticias  */
+      setNoticias(datos.results)
+    } catch (error) {
+      console.log(error);
+     /*  {
+        <Alert key={variant} variant={warnign}>
+          Se rompeiron las maquinas impresoras y eso ya es noticia, volvé pronto
+        </Alert>
+      } */
+    }
+
   }
 /*  */
   return (
