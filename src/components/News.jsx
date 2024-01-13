b047ee0af29b755521c63d8c0dcf5c052ccea35a
CosmeNicolas <nicomas7590@gmail.com>
2024-01-13 18:59:02 -0300

    agregue estilos a la card

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import { Container, Image } from 'react-bootstrap';
+import { Container, Image, Card, Button, Col, Row, ListGroup } from 'react-bootstrap';
 
-const News = ({noticias}) => {
+const News = ({ noticias }) => {
   return (
-    <Container>
-      {
-        noticias.map(noticia => (
-          <Card key={noticia.article_id} style={{ width: '18rem' }}>
-           <Image src={noticia.image_url} /> 
-            <Card.Body>
-              <Card.Title>{noticia.title}</Card.Title>
-              <Card.Text>
-               {noticia.description}
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        ))
-      }
+    <>    <Container className=''>
+      <hr />
+      <h1 className='text-center' >It's Happening  📰</h1>
+      <br />
+      <Row xs={1} md={2} lg={4}>
+        {
+          noticias.map(noticia => (
+            <Col className='d-flex justify-content-center' key={noticia.article_id}>
+              <Card className='my-3' >
+                <Image src={noticia.image_url} className='img-fluid img-new' />
+                <Card.Body>
+                  <Card.Title>{noticia.title}</Card.Title>
+                  <Card.Text>
+                    {noticia.description}
+                    <ListGroup className="list-group-flush">
+                      <ListGroup.Item>
+                        Author: <em>{noticia.creator} - {noticia.country}</em>
+                      </ListGroup.Item>
+                    </ListGroup >
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer className='text-center fondo-footer'>
+                  <Button variant="dark" >Read</Button>
+                </Card.Footer>
+              </Card>
+            </Col>
+          ))
+        }
+
+      </Row>
     </Container>
+    </>
+
   );
 };
 
