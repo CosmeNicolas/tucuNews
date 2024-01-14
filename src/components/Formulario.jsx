ccbae820ea0a40b946b4e1d74830fab62b2afecc
CosmeNicolas <nicomas7590@gmail.com>
2024-01-14 19:38:54 -0300

    agregue las categorias faltantes que brinda la documentacion de la API

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -22,6 +22,20 @@ const Formulario = ({ noticias, consultaApi, setElegirCategoria, elegirCategoria
         <option>Category</option>
         <option value="business">Business</option>
         <option value="crime">Crime</option>
+        <option value="domestic">Domestic</option>
+        <option value="education">Education</option>
+        <option value="entertainment">Entertainment</option>
+        <option value="environment">Environment</option>
+        <option value="food">Food</option>
+        <option value="health">Health</option>
+        <option value="other">Other</option>
+        <option value="politics">Politics</option>
+        <option value="science">Science</option>
+        <option value="sport">Sport</option>
+        <option value="technology">Tecnology</option>
+        <option value="top">Top</option>
+        <option value="tourism">Tourism</option>
+        <option value="world">World</option>
       </Form.Select>
       <News noticias={noticias} />
     </>
