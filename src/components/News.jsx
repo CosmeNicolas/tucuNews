2423d781250b51f9bef73d7919d409a0f9c8ba77
CosmeNicolas <nicomas7590@gmail.com>
2024-01-14 21:28:42 -0300

    deje comentado el contenido de la noticia

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,28 +7,29 @@ const News = ({ noticias }) => {
   return (
     <>
       <Container>
-
+       
         <Row xs={1} md={2} lg={4} className='justify-content-center '>
-          {
-            Array.isArray(noticias) ? (
-              noticias.map(noticia => (
-                <Col className='d-flex justify-content-center' key={noticia.article_id}>
-                  <Card className='my-3' >
-                    <Image src={noticia.image_url} className='img-fluid img-new' />
-                    <Card.Body>
-                      <Card.Title>{noticia.title}</Card.Title>
-                      <p>{noticia.content}</p>
-                      <p>Author: <em>{noticia.creator} - {noticia.country}</em></p>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              ))
-            ) : (
-              <Alert className='mt-3' variant="light">
-                There aren't any news available
-              </Alert>
-            )
-          }
+          
+            {
+              Array.isArray(noticias) ? (
+                noticias.map(noticia => (
+                  <Col className='d-flex justify-content-center' key={noticia.article_id}>
+                    <Card className='my-3' >
+                      <Image src={noticia.image_url} className='img-fluid img-new' />
+                      <Card.Body>
+                        <Card.Title>{noticia.title}</Card.Title>
+                        {/* <p>{noticia.content}</p> */}
+                        <p>Author: <em>{noticia.creator} - {noticia.country}</em></p>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))
+              ) : (
+                <Alert className='mt-3'  variant="light">
+                  There aren't any news available
+                </Alert>
+              )
+            }
         </Row>
       </Container>
     </>
