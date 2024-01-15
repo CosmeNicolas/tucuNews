904c28f38d9e9d607e3e16a9362c0d48b44b1eeb
CosmeNicolas <nicomas7590@gmail.com>
2024-01-14 21:06:08 -0300

    agregue el contenido de las noticias

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,6 +7,7 @@ const News = ({ noticias }) => {
   return (
     <>
       <Container>
+
         <Row xs={1} md={2} lg={4} className='justify-content-center '>
           {
             Array.isArray(noticias) ? (
@@ -16,7 +17,7 @@ const News = ({ noticias }) => {
                     <Image src={noticia.image_url} className='img-fluid img-new' />
                     <Card.Body>
                       <Card.Title>{noticia.title}</Card.Title>
-                      {/* <p>{noticia.content}</p> */}
+                      <p>{noticia.content}</p>
                       <p>Author: <em>{noticia.creator} - {noticia.country}</em></p>
                     </Card.Body>
                   </Card>
