8737815edb4e2432a721d82195a84df9865c3f65
CosmeNicolas <nicomas7590@gmail.com>
2024-01-14 21:05:14 -0300

    saque el boton de leer notta completa

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,5 @@
-import { Container, Image, Card, Col, Row, Alert, Button } from 'react-bootstrap';
+import { Container, Image, Card, Col, Row, Alert } from 'react-bootstrap';
+
 
 
 const News = ({ noticias }) => {
@@ -6,34 +7,27 @@ const News = ({ noticias }) => {
   return (
     <>
       <Container>
-       
         <Row xs={1} md={2} lg={4} className='justify-content-center '>
-          
-            {
-              Array.isArray(noticias) ? (
-                noticias.map(noticia => (
-                  <Col className='d-flex justify-content-center' key={noticia.article_id}>
-                    <Card className='my-3' >
-                      <Image src={noticia.image_url} className='img-fluid img-new' />
-                      <Card.Body>
-                        <Card.Title>{noticia.title}</Card.Title>
-                        {/* <p>{noticia.content}</p> */}
-                        <p>Author: <em>{noticia.creator} - {noticia.country}</em></p>
-                      <Card.Footer className='text-center bg-dark'>
-                        <Button variant="dark" >
-                          Read all note
-                        </Button>
-                      </Card.Footer>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                ))
-              ) : (
-                <Alert className='mt-3'  variant="light">
-                  There aren't any news available
-                </Alert>
-              )
-            }
+          {
+            Array.isArray(noticias) ? (
+              noticias.map(noticia => (
+                <Col className='d-flex justify-content-center' key={noticia.article_id}>
+                  <Card className='my-3' >
+                    <Image src={noticia.image_url} className='img-fluid img-new' />
+                    <Card.Body>
+                      <Card.Title>{noticia.title}</Card.Title>
+                      {/* <p>{noticia.content}</p> */}
+                      <p>Author: <em>{noticia.creator} - {noticia.country}</em></p>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))
+            ) : (
+              <Alert className='mt-3' variant="light">
+                There aren't any news available
+              </Alert>
+            )
+          }
         </Row>
       </Container>
     </>
